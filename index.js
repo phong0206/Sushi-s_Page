21b2d1afcbacc102c39b64dc621ff0e8245f7d6d
Phong <vanphong02062002@gmail.com>
2023-03-22 09:02:44 +0700

    9h45_work

---
+++ index.js
@@ -0,0 +1,36 @@
+
+//navbar scroll
+window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("slide-down-btn").style.top = "0px";
+            } else {
+                document.getElementById("slide-down-btn").style.top = "0px";
+            }
+}
+
+// slideshow
+ var slideIndex = 1;
+            showSlides(slideIndex);
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+            function currentSlide(n) {
+                showSlides(slideIndex = n);
+            }
+            function showSlides(n) {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                var dots = document.getElementsByClassName("dot");
+                if (n > slides.length) { slideIndex = 1 }
+                if (n < 1) { slideIndex = slides.length }
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex - 1].style.display = "block";
+                
+            }
